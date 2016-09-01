6bac259aff1505e2c28fcf5c3bdfca165622e738
Michael Williams <michael.williams@enspiral.com>
2016-09-01 23:09:33 +1200

    first pass at pull-stream export

---
+++ pull.js
@@ -0,0 +1,27 @@
+/**
+ * @module  web-audio-stream/writable
+ *
+ * Write stream data to web-audio.
+ */
+'use strict';
+
+
+var pull = require('pull-stream/pull');
+var asyncMap = require('pull-stream/throughs/async-map');
+var drain = require('pull-stream/sinks/drain');
+var createWriter = require('./writer');
+
+module.exports = WAAStream;
+
+
+/**
+ * @constructor
+ */
+function WAAStream (node, options) {
+	let write = createWriter(node, options);
+
+	return pull(
+		asyncMap(write),
+		drain()
+	);
+}
